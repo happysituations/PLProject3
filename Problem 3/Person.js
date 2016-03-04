e228d4ff992bcb8287e4692e207425c9d969ada4
Happy Situ <happysitu@utexas.edu>
2016-03-04 15:11:12 -0600

    js

--- Problem 3/Person.js
+++ Problem 3/Person.js
@@ -121,7 +121,7 @@ for (var key in Object.getPrototypeOf(socrates)) {
 // View local and inherited properties.
 document.writeln("<BR>" + "Local and inherited properties are: <BR>");
 for (var key in Object.getPrototypeOf(socrates)) {
-        document.writeln('socrates: ' + key + " -> " + Object.getPrototypeOf(socrates)[key] + "<BR>");
+    document.writeln('socrates: ' + key + " -> " + Object.getPrototypeOf(socrates)[key] + "<BR>");
 }
 
 document.writeln("<BR>");
@@ -133,13 +133,25 @@ document.writeln("<BR>");
 a1.speak(a1);
 a1.speak(socrates);
 
-$(document).ready(function(){
-    $("#person_form").click(function(){
-        var cust = $("#person_form");
-        var firstName = $("#first_name");
-        var lastName = $("#last_name");
-    $("button").click(function(){
-        $("")
-    })
+$(document).ready(function() {
+    $("#accordion").on("click", "h2", function(){
+        $(this).toggleClass("minus");
+        if($(this).attr("class") != "minus"){
+            $(this).next().hide();
+        }
+        else{
+            $(this).next.show();
+        }
     });
-})
+    $("#add").click(function(){
+        var html = "";
+        html += "<h2><a href='#'>" + $("#name").val() + "</a></h2>";
+        html += "<div><h3>" + $("position").val() + "</h3>";
+        html += "<p>" + $("#description").val() + "</p><div>";
+        $("#accordion").append(html);
+        $("#name").val("");
+        $("position").val("");
+        $("#description").val("");
+    });
+});
+
